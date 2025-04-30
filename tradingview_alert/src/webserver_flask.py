65a5fa8083562e14976407db5f2d6eb06c4f16c8
iblea <iblea@iasdf.com>
2025-05-01 03:18:57 +0900

    tradingview POST webhook 추가

--- tradingview_alert/src/webserver_flask.py
+++ tradingview_alert/src/webserver_flask.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from werkzeug.serving import WSGIRequestHandler
+
+from msg import msg_queue
 
 
 app = Flask(__name__)
@@ -14,6 +17,25 @@ def index():
     return jsonify({"message": "Hello, World!"})
 
 
-@app.route('/tradingview/alert')
+@app.route('/tradingview/alert', methods=['POST'])
 def tradingview_alert():
-    return jsonify({"message": "Hello, World!"})
+    global msg_queue
+    # POST 요청의 JSON 데이터 가져오기
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "데이터가 없습니다"}), 400
+
+    # 여기서 받은 데이터 처리
+    # print("받은 데이터:", data)
+
+    stat = msg_queue.push(str(data))
+    if not stat:
+        print("Error: msg_queue.push()")
+        return jsonify({"error": "메시지 전달 실패"}), 500
+
+    # 성공 응답
+    return jsonify({
+        "status": "success",
+        "received_data": data
+    }), 200
