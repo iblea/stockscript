c4ce0882d6a5ac028fa19e133ce53a16fdb47827
iblea <iblea@iasdf.com>
2025-05-01 03:17:18 +0900

    feat: 봇 config 추가

--- tradingview_alert/src/config.py
+++ tradingview_alert/src/config.py
@@ -7,8 +7,13 @@ SCRIPT_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_FILE: str = "conf/config.json"
 CONFIG_PATH: str = os.path.join(SCRIPT_DIR, CONFIG_FILE)
 
-CERT_FILE: str = "cert/server.crt"
-KEY_FILE: str = "cert/server.key"
+HTTPS_CERT_FILE: str = "cert/server.crt"
+HTTPS_KEY_FILE: str = "cert/server.key"
+
+HTTPS_CERT_PATH: str = os.path.join(SCRIPT_DIR, HTTPS_CERT_FILE)
+HTTPS_KEY_PATH: str = os.path.join(SCRIPT_DIR, HTTPS_KEY_FILE)
+
+
 
 data: dict = None
 
@@ -33,27 +38,27 @@ def check_config() -> bool:
     if data is None:
         return False
 
-    obj: any = data.get("trade")
+    obj: any = data.get("trade", None)
     if obj is None or obj == "":
         print("Error: config.json에 trade 설정이 없습니다.")
         return False
 
-    rest_api: any = data.get("rest_api")
+    rest_api: any = data.get("rest_api", None)
     if rest_api is None:
         print("Error: config.json에 rest_api 설정이 없습니다.")
         return False
 
-    use_api: str = rest_api.get("use")
+    use_api: str = rest_api.get("use", None)
     if use_api is None:
         print("Error: config.json에 rest_api.use 설정이 없습니다.")
         return False
     elif use_api == "kiwoom":
-        obj = rest_api.get("kiwoom")
+        obj = rest_api.get("kiwoom", None)
         if obj is None or obj == "":
             print("Error: config.json에 rest_api.kiwoom (키움증권 api) 설정이 없습니다.")
             return False
     elif use_api == "koreainvestment":
-        obj = rest_api.get("koreainvestment")
+        obj = rest_api.get("koreainvestment", None)
         if obj is None or obj == "":
             print("Error: config.json에 rest_api.koreainvestment (한국투자증권 api) 설정이 없습니다.")
             return False
@@ -61,19 +66,19 @@ def check_config() -> bool:
         print("Error: rest_api.use 값이 유효하지 않습니다.")
         return False
 
-    flask: any = data.get("flask")
+    flask: any = data.get("flask", None)
     if flask is None:
         print("Error: config.json에 flask 웹서버 설정이 없습니다.")
         return False
 
-    http_port: any = flask.get("http_port")
+    http_port: any = flask.get("http_port", None)
     if http_port is None:
         print("Error: config.json에 flask.http_port 설정이 없습니다.")
         return False
     elif http_port < 0 or http_port > 65535:
         print("Error: config.json에 flask.http_port 값이 유효하지 않습니다. 포트 값은 0~65535 사이여야 합니다.")
         return False
-    https_port: any = flask.get("https_port")
+    https_port: any = flask.get("https_port", None)
     if https_port is None:
         print("Error: config.json에 flask.https_port 설정이 없습니다.")
         return False
@@ -85,19 +90,74 @@ def check_config() -> bool:
         print("Error: flask.http_port와 flask.https_port는 다른 값이어야 합니다.")
         return False
 
+    bot_data = data.get("bot", None)
+    if bot_data is None:
+        print("Error: config.json에 bot 설정이 없습니다.")
+        return False
+
+    obj = bot_data.get("use_discord", None)
+    if obj is None:
+        print("Error: config.json에 bot.use_discord 설정이 없습니다.")
+        return False
+
+    obj = bot_data.get("use_telegram", None)
+    if obj is None:
+        print("Error: config.json에 bot.use_telegram 설정이 없습니다.")
+        return False
+
+    discord_data = bot_data.get("discord", None)
+    if discord_data is None:
+        print("Error: config.json에 bot.discord 설정이 없습니다.")
+        return False
+
+    obj = discord_data.get("token", None)
+    if obj is None:
+        print("Error: config.json에 bot.discord.token 설정이 없습니다.")
+        return False
+
+    obj = discord_data.get("server_id", 0)
+    if obj == 0:
+        print("Error: config.json에 bot.discord.server_id 설정이 없습니다.")
+        return False
+
+    obj = discord_data.get("channel_id", 0)
+    if obj == 0:
+        print("Error: config.json에 bot.discord.channel_id 설정이 없습니다.")
+        return False
+
+    obj = discord_data.get("alert_interval", None)
+    if obj is None:
+        print("Error: config.json에 bot.discord.alert_interval 설정이 없습니다.")
+        return False
+
+    telegram_data = bot_data.get("telegram", None)
+    if telegram_data is None:
+        print("Error: config.json에 bot.telegram 설정이 없습니다.")
+        return False
+
+    obj = telegram_data.get("token", None)
+    if obj is None:
+        print("Error: config.json에 bot.telegram.token 설정이 없습니다.")
+        return False
+
+    obj = telegram_data.get("chat_id", 0)
+    if obj == 0:
+        print("Error: config.json에 bot.telegram.chat_id 설정이 없습니다.")
+        return False
+
+    obj = telegram_data.get("alert_interval", None)
+    if obj is None:
+        print("Error: config.json에 bot.telegram.alert_interval 설정이 없습니다.")
+        return False
+
     return True
 
-
-
 def check_cert() -> bool:
-    cert_path: str = os.path.join(SCRIPT_DIR, CERT_FILE)
-    key_path: str = os.path.join(SCRIPT_DIR, KEY_FILE)
-
-    if not os.path.exists(cert_path):
-        print(f"Error: SSL 인증서 파일이 존재하지 않습니다. {CERT_FILE} 파일을 생성해주세요.")
+    if not os.path.exists(HTTPS_CERT_PATH):
+        print(f"Error: SSL 인증서 파일이 존재하지 않습니다. {HTTPS_CERT_FILE} 파일을 생성해주세요.")
         return False
-    if not os.path.exists(key_path):
-        print(f"Error: SSL 키 파일이 존재하지 않습니다. {KEY_FILE} 파일을 생성해주세요.")
+    if not os.path.exists(HTTPS_KEY_PATH):
+        print(f"Error: SSL 키 파일이 존재하지 않습니다. {HTTPS_KEY_FILE} 파일을 생성해주세요.")
         return False
 
     return True
